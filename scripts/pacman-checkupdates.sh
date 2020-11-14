588dc4d030f71d880ef8d8bbd20d8ec578942130
0mirror <tiltalot5@gmail.com>
2020-11-14 14:01:01 +0200

    updated pacman-checkupdates.sh

--- scripts/pacman-checkupdates.sh
+++ scripts/pacman-checkupdates.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-[[ $(ping -c 1 -q archlinux.org >&/dev/null; echo $?) == 0 ]] && checkupdates | wc -l | tail -n1
+[[ $(ping -c 1 -q archlinux.org >&/dev/null; echo $?) == 0 ]] && checkupdates | wc -l | tail -n1 | awk '{ print ($1 != 0) ? $1 : "" }'
