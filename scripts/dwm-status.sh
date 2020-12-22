dec9d52a4efad3d73800919d122dc5ebfdaeec78
0mirror <tiltalot5@gmail.com>
2020-12-22 22:25:15 +0200

    dwm-status.sh wifi fixed

--- scripts/dwm-status.sh
+++ scripts/dwm-status.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 setup () {
+
 	mic=$([[ $(pulsemixer --id source-1 --get-mute) == 1 ]] && printf "[MIC: muted]")
 	volume=$([[ $(pulsemixer --id sink-0 --get-mute) == 1 ]] && printf "[VOL: muted]" || pulsemixer --id sink-0 --get-volume | awk '{print "[VOL: "$1"%]"}') 
 	mem=$(free -m | awk '/Mem/ {print "[MEM: "$3+$5"]"}')
-	con=$(nmcli connection show | awk '/con/ {print "["$5"]"}')
+	con=$(nmcli device show wlp1s0 | grep -q "disconnected" && printf "[wifi off]" || printf "[wifi on]") 
 	date=$(date "+[%H:%M %d-%m-%Y]")
+	battery=$(cat /sys/class/power_supply/BAT0/capacity | awk '{printf "[BAT: "$1"%]"}')
+
 }
 
 while setup
 do
-	xsetroot -name "$mic $volume $mem $con $date"
+	xsetroot -name "$mic $volume $mem $con $date $battery"
 	sleep 1
 done
