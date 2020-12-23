1bbd382c3e72b0e71de1241fa06aed5165624122
0mirror <tiltalot5@gmail.com>
2020-12-23 19:50:49 +0200

    dwm-status.sh add backlight

--- scripts/dwm-status.sh
+++ scripts/dwm-status.sh
@@ -4,6 +4,7 @@ setup () {
 
 	mic=$([[ $(pulsemixer --id source-1 --get-mute) == 1 ]] && printf "[MIC: muted]")
 	volume=$([[ $(pulsemixer --id sink-0 --get-mute) == 1 ]] && printf "[VOL: muted]" || pulsemixer --id sink-0 --get-volume | awk '{print "[VOL: "$1"%]"}') 
+	backlight=$(xbacklight -get | awk '{split($0,a,"."); print "[BL: "a[1]"%]"}')
 	mem=$(free -m | awk '/Mem/ {print "[MEM: "$3+$5"]"}')
 	con=$(nmcli device show wlp1s0 | grep -q "disconnected" && printf "[wifi off]" || printf "[wifi on]") 
 	date=$(date "+[%H:%M %d-%m-%Y]")
@@ -13,6 +14,6 @@ setup () {
 
 while setup
 do
-	xsetroot -name "$mic $volume $mem $con $date $battery"
+	xsetroot -name "$mic $volume $backlight $mem $con $date $battery"
 	sleep 1
 done
